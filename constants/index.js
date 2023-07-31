6ae30e0f574eb6725753006bd52e3568d9f6e5de
lll1lll2ll3ll4 <119754307+lll1lll2ll3ll4@users.noreply.github.com>
2023-07-31 02:24:57 +0200

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xfdef657ebee8e17566ab749e9589222970603224";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
